acdc512e53d7cb7e99c7bac1d57d4d6c778a21fd
it-school-admin <alexandrov@it-school.su>
2020-11-22 05:26:51 +0400

    Календарь с редактированием

--- modules/web/src/su/itschool/guru/web/screens/session/SessionBrowse.java
+++ modules/web/src/su/itschool/guru/web/screens/session/SessionBrowse.java
@@ -1,9 +1,11 @@
 package su.itschool.guru.web.screens.session;
 
 import com.haulmont.cuba.gui.components.Calendar;
+import com.haulmont.cuba.gui.components.EditorScreenFacet;
 import com.haulmont.cuba.gui.screen.*;
 import su.itschool.guru.entity.Session;
 
+import javax.inject.Inject;
 import java.time.LocalDateTime;
 
 @UiController("guru_Session.browse")
@@ -11,8 +13,12 @@ import java.time.LocalDateTime;
 @LookupComponent("sessionsTable")
 @LoadDataBeforeShow
 public class SessionBrowse extends StandardLookup<Session> {
+    @Inject
+    private EditorScreenFacet<Session, SessionEdit> sessionEditorScreen;
+
     @Subscribe("sessionsCalendar")
     public void onSessionsCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<LocalDateTime> event) {
-        
+        sessionEditorScreen.setEntityProvider(()-> (Session) event.getEntity());
+        sessionEditorScreen.show();
     }
 }
