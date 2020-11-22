128bbeb08c9929f3f78b87ad8df018ae6817fc4a
it-school-admin <alexandrov@it-school.su>
2020-11-22 05:10:14 +0400

    Добавлен календарь

--- modules/web/src/su/itschool/guru/web/screens/session/SessionBrowse.java
+++ modules/web/src/su/itschool/guru/web/screens/session/SessionBrowse.java
@@ -1,11 +1,18 @@
 package su.itschool.guru.web.screens.session;
 
+import com.haulmont.cuba.gui.components.Calendar;
 import com.haulmont.cuba.gui.screen.*;
 import su.itschool.guru.entity.Session;
+
+import java.time.LocalDateTime;
 
 @UiController("guru_Session.browse")
 @UiDescriptor("session-browse.xml")
 @LookupComponent("sessionsTable")
 @LoadDataBeforeShow
 public class SessionBrowse extends StandardLookup<Session> {
+    @Subscribe("sessionsCalendar")
+    public void onSessionsCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<LocalDateTime> event) {
+        
+    }
 }
