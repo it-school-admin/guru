509fc4cbecc3cbf0f92c84f5cb036dc917366903
it-school-admin <alexandrov@it-school.su>
2020-12-20 23:02:00 +0400

    Create week days.

---
+++ modules/core/src/su/itschool/guru/service/WeekDaysServiceBean.java
@@ -0,0 +1,25 @@
+package su.itschool.guru.service;
+
+import com.haulmont.cuba.core.global.DataManager;
+import org.springframework.stereotype.Service;
+import su.itschool.guru.entity.WeekDay;
+
+import javax.inject.Inject;
+
+@Service(WeekDaysService.NAME)
+public class WeekDaysServiceBean implements WeekDaysService {
+
+    @Inject
+    private DataManager dataManager;
+
+    @Override
+    public void createWeekDays(String... weekDayName) {
+        // todo move to init application or to SQL script
+        for(int i = 1; i < weekDayName.length+1; i++) {
+            WeekDay weekDay = dataManager.create(WeekDay.class);
+            weekDay.setDayNumber(i);
+            weekDay.setDayName(weekDayName[i]);
+            dataManager.commit(weekDay);
+        }
+    }
+}
---
+++ modules/global/src/su/itschool/guru/service/WeekDaysService.java
@@ -0,0 +1,7 @@
+package su.itschool.guru.service;
+
+public interface WeekDaysService {
+    String NAME = "guru_WeekDaysService";
+
+    void createWeekDays(String ... weekDayName);
+}
