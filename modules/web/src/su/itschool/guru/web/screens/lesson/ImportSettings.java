15e803097cf513a7557cbd0ba58ad6bb643a20a2
it-school-admin <Dm0HY4sUiH>
2020-12-29 01:56:18 +0400

    import from IrTech. Part2

---
+++ modules/web/src/su/itschool/guru/web/screens/lesson/ImportSettings.java
@@ -0,0 +1,7 @@
+package su.itschool.guru.web.screens.lesson;
+
+import com.haulmont.cuba.core.entity.FileDescriptor;
+
+public class ImportSettings {
+    public FileDescriptor filename;
+}
--- modules/web/src/su/itschool/guru/web/screens/lesson/LessonBrowseForDispatcher.java
+++ modules/web/src/su/itschool/guru/web/screens/lesson/LessonBrowseForDispatcher.java
@@ -1,18 +1,43 @@
 package su.itschool.guru.web.screens.lesson;
 
+import com.haulmont.cuba.core.entity.FileDescriptor;
+import com.haulmont.cuba.gui.Dialogs;
+import com.haulmont.cuba.gui.app.core.inputdialog.DialogActions;
+import com.haulmont.cuba.gui.app.core.inputdialog.DialogOutcome;
 import com.haulmont.cuba.gui.app.core.inputdialog.InputParameter;
 import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.screen.*;
 import su.itschool.guru.entity.Lesson;
+
+import javax.inject.Inject;
+
+import static com.haulmont.cuba.gui.app.core.inputdialog.DialogActions.OK_CANCEL;
+import static com.haulmont.cuba.gui.app.core.inputdialog.DialogOutcome.OK;
 
 @UiController("guru_Lesson.browse-for-dispatcher")
 @UiDescriptor("lesson-browse-for-dispatcher.xml")
 @LookupComponent("lessonsTable")
 @LoadDataBeforeShow
 public class LessonBrowseForDispatcher extends StandardLookup<Lesson> {
+    @Inject
+    private Dialogs dialogs;
+
     @Subscribe("ImportBtn")
     public void onImportBtnClick(Button.ClickEvent event) {
- //TODO       InputParameter.fileParameter()
+        ImportSettings importSettings = new ImportSettings();
+
+        dialogs.createInputDialog(this)
+               .withCaption("Мастер импортирования данных из ИрТех")
+               .withParameters(InputParameter.fileParameter("filename"))
+               .withActions(OK_CANCEL)
+               .withCloseListener(closeEvent->{
+                   if(closeEvent.closedWith(OK))
+                   {
+                       importSettings.filename = closeEvent.getValue("filename");
+                   }
+               })
+               .show();
+        //dialogs.createOptionDialog()
 
     }
 }
