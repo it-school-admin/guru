6ea4c882e874c655de6f77fc3655bb2a5eda0c17
it-school-admin <Dm0HY4sUiH>
2021-01-04 23:26:54 +0400

    LessonsPlanningItem change structure.

--- modules/global/src/su/itschool/guru/entity/LessonsPlanningItem.java
+++ modules/global/src/su/itschool/guru/entity/LessonsPlanningItem.java
@@ -8,14 +8,19 @@ import javax.validation.constraints.NotNull;
 
 @Table(name = "GURU_LESSONS_PLANNING_ITEM")
 @Entity(name = "guru_LessonsPlanningItem")
-@NamePattern("%s %s|groupOfLearning,hoursPerWeek")
+@NamePattern("%s|hoursPerWeek")
 public class LessonsPlanningItem extends StandardEntity {
     private static final long serialVersionUID = 3761661617055118840L;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "GROUP_OF_LEARNING_ID")
     @NotNull
-    private GroupForLesson groupOfLearning;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "SCHOOL_CLASS_ID")
+    private SchoolClass schoolClass;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "SUBJECT_ID")
+    private Subject subject;
 
     @Column(name = "HOURS_PER_WEEK", nullable = false)
     @NotNull
@@ -23,6 +28,22 @@ public class LessonsPlanningItem extends StandardEntity {
 
     @Column(name = "IR_TECH_ID", unique = true)
     private Integer irTechID;
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public SchoolClass getSchoolClass() {
+        return schoolClass;
+    }
+
+    public void setSchoolClass(SchoolClass schoolClass) {
+        this.schoolClass = schoolClass;
+    }
 
     public void setHoursPerWeek(Integer hoursPerWeek) {
         this.hoursPerWeek = hoursPerWeek;
@@ -38,14 +59,6 @@ public class LessonsPlanningItem extends StandardEntity {
 
     public void setIrTechID(Integer irTechID) {
         this.irTechID = irTechID;
-    }
-
-    public GroupForLesson getGroupOfLearning() {
-        return groupOfLearning;
-    }
-
-    public void setGroupOfLearning(GroupForLesson groupOfLearning) {
-        this.groupOfLearning = groupOfLearning;
     }
 
 }
