862a697ceaab1ef4652accbfd1f452ed867755b7
it-school-admin <Dm0HY4sUiH>
2021-01-10 12:14:01 +0400

    Making timetable UI(in progress).

--- modules/web/src/su/itschool/guru/web/screens/TimetableMaking.java
+++ modules/web/src/su/itschool/guru/web/screens/TimetableMaking.java
@@ -1,12 +1,16 @@
 package su.itschool.guru.web.screens;
 
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.Screen;
 import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
 import su.itschool.guru.entity.SchoolClass;
+import su.itschool.guru.entity.TimeTableTemplateItem;
 import su.itschool.guru.entity.TimetableTemplate;
+import su.itschool.guru.entity.enums.WeekDay;
 
 import javax.inject.Inject;
 
@@ -17,10 +21,29 @@ public class TimetableMaking extends Screen {
     private CollectionLoader<SchoolClass> schoolClassesDl;
     @Inject
     private CollectionLoader<TimetableTemplate> timetableTemplatesDl;
+    @Inject
+    private LookupField<TimetableTemplate> timetableTemplateLookUpField;
+    @Inject
+    private LookupField<WeekDay> weekDayLookUpField;
+    @Inject
+    private CollectionLoader<TimeTableTemplateItem> timeTableTemplateItemsDl;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
         schoolClassesDl.load();
         timetableTemplatesDl.load();
+    }
+
+    @Subscribe("timetableTemplateLookUpField")
+    public void onTimetableTemplateLookUpFieldValueChange(HasValue.ValueChangeEvent<TimetableTemplate> event) {
+        tryToLoadTemplateItems();
+    }
+
+    private void tryToLoadTemplateItems() {
+        if(timetableTemplateLookUpField.getValue() != null &&
+                weekDayLookUpField.getValue() != null)
+        {
+            timeTableTemplateItemsDl.load();
+        }
     }
 }
