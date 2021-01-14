0daf1e735653f6f2245fb8d0fd25fe5472270beb
it-school-admin <Dm0HY4sUiH>
2021-01-14 20:59:09 +0400

    Some changes.

--- modules/core/db/update/postgres/21/01/210114-1-updateLessonGridItems.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM guru_lessons_grid_item;
--- modules/core/db/update/postgres/21/01/210114-2-updateClass.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM guru_time_table_template_item;
-DELETE FROM guru_lessons_planning_item;
-DELETE FROM guru_group_for_lesson;
-DELETE FROM guru_school_class;
